846ce8eebdcb715590f2add9ef22a8345ea4cee5
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-10-25 16:17:18 +0800

    1025

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function toggle() {
-  let sec = document.getElementById("sec");
-  let nav = document.getElementById("navigation");
-  sec.classList.toggle("active");
-  nav.classList.toggle("active");
+function toggleMenu() {
+  const menuToggle = document.querySelector(".toggle");
+  const sidebar = document.querySelector(".sidebar");
+  menuToggle.classList.toggle("active");
+  sidebar.classList.toggle("active1");
 }

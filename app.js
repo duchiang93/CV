da9f223246cedc2fe834a89f52e42418399b9c0e
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-11-03 16:08:25 +0800

    1103

--- app.js
+++ app.js
@@ -4,3 +4,34 @@ function toggleMenu() {
   menuToggle.classList.toggle("active");
   sidebar.classList.toggle("active");
 }
+
+let skills = document.getElementById("skills");
+let percent = document.querySelectorAll(".percent");
+let dot = document.querySelectorAll(".dot");
+
+// window.addEventListener("scroll", () => {
+//   let sectionPos = skills.getBoundingClientRect().top;
+//   let screenPos = window.innerHeight;
+
+//   if (sectionPos < screenPos - 500) {
+//     console.log("show progress");
+//     percent.classList.add(".dot_animate");
+//   } else {
+//     console.log("hide progress");
+//   }
+// });
+
+function skillAnimate() {
+  let sectionPos = skills.getBoundingClientRect().top;
+  let screenPos = window.innerHeight;
+
+  if (sectionPos < screenPos - 500) {
+    console.log("show progress");
+    let card = document.querySelectorAll(".card");
+    for (let i = 0; i < card.length; i++) card[i].classList.add("active");
+    window.removeEventListener("scroll", skillAnimate);
+  } else {
+    console.log("hide progress");
+  }
+}
+window.addEventListener("scroll", skillAnimate);

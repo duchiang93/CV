f5adbf18d5867f6588a544665487197e6f90fc02
DESKTOP-EQSEK8E\Guest1 <duchiang93@gmail.com>
2022-11-08 15:52:21 +0800

    1108

---
+++ js/index.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".title", {
+  duration: 2500,
+  origin: "bottom",
+  distance: "-80px",
+});
+
+sr.reveal(".scroll-home", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "-20px",
+});
+
+sr.reveal(".about-text", {
+  duration: 3000,
+  origin: "right",
+  distance: "-100px",
+});
+
+sr.reveal(".about-IMG", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
+
+sr.reveal(".project-scroll", {
+  duration: 3000,
+  origin: "top",
+  distance: "-50px",
+});
+
+sr.reveal(".contactinfo", {
+  duration: 3000,
+  origin: "right",
+  distance: "-100px",
+});
+
+sr.reveal(".contactForm", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
